8bbf6efa2e8425b5a62c308f212ffdb0ada9439b
969022607@qq.com <969022607@qq.com>
2019-03-15 18:42:11 +0800

    login

--- js/user/login.js
+++ js/user/login.js
@@ -1,24 +1,68 @@
 $(function(){
+
+
+	// 验证用户手机格式
 	 $(".phone").blur(function(){
-		 let regg= /^1[34578]\d{9}$/;  /*用户手机号*/
+		 let reg= /^1[34578]\d{9}$/;  /*用户手机号*/
 		 let $phone=$(this)
-		 verify(regg,$phone,"手机号")
+		 
+		   if($phone.val()==""){
+		   			 $phone.parent().prev().addClass("mistake").html("手机号不能为空")
+		   			 return _state_.push(false);
+		   }else if(!reg.test($phone.val())){
+		   			 $phone.parent().prev().addClass("mistake").html("请输入正确的手机号~")
+		   			 return _state_.push(false);
+		   }else{
+							$phone.parent().prev().removeClass("mistake").html("")
+							if($(this).parent().siblings(".from-group").children(".pptt").val().length>0){
+                  $(".btn").addClass("active").removeClass("dis").attr("disabled",false)
+							}else{
+								  $(".btn").addClass("dis").removeClass("active").attr("disabled","disabled")
+							}
+		   			 return true;
+		   }
 	 })
+	 // 验证用户密码格式
 	 $(".pwd").blur(function(){
-		 let regg=/^[a-zA-Z0-9]{6,16}$/;; /*用户密码*/
+		 let reg=/^[a-zA-Z0-9]{6,16}$/;; /*用户密码*/
 		 let $pwd=$(this)
-		 verify(regg,$pwd,"密码")
-	 })
-     function verify(reg,pro,dd){
-		 if(pro.val()==""){
-			 pro.parent().prev().addClass("mistake").html(`${dd}`).show()
+		 if($pwd.val()==""){
+			 $pwd.parent().prev().addClass("mistake").html("用户密码不能为空")
 			 return false;
-		 }else if(!reg.test(pro.val())){
-			 pro.parent().prev().addClass("mistake").html(`${dd}`).show()
+		 }else if(!reg.test($pwd.val())){
+			 $pwd.parent().prev().addClass("mistake").html("用户名密码格式错误 6~16位~")
 			 return false;
 		 }else{
-			 pro.parent().prev().removeClass("mistake").html("").hide();
+			 $pwd.parent().prev().removeClass("mistake").html("")
+			   if($(this).parent().siblings(".from-group").children(".pptt").val().length>0){
+			  	$(".btn").addClass("active").removeClass("dis").attr("disabled",false)
+		}else{
+			  	$(".btn").addClass("dis").removeClass("active").attr("disabled","disabled")
+		}
 			 return true;
 		 }
+	 })
+	 
+	 // 返回上一页
+	     function returnFun(){
+	 	/*返回上一页*/
+	 	if($('.headWrap_lb .returnBut_lb').attr('data-num') == 1 || $('.headWrap_lb .returnBut_lb').attr('data-num') == undefined ){
+	 		window.history.back();
+	 		console.log("返回上一页");
+	 	}else {
+	 		/*页面跳转*/
+	 		window.location.href = $('.headWrap_lb .returnBut_lb').attr('data-num');
+	 	}
+	 	return false;
 	 }
+	
+	//  登录
+	  $(".btn").click(function(){
+			var input = $("input")
+			console.log(input)
+			for (input in item){
+				console.log(item)
+			}
+		})
+
 	})
